14f67a40898775849a1671f39ca839e743317977
peufee <paulaescri24@gmail.com>
2025-03-10 12:35:02 +0100

    Cambio a prueba y creacion api definitiva

---
+++ PersonalGoalsApi/PersonalGoalsApi/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalGoalsApi.Models;
+
+namespace PersonalGoalsApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Goal> Goals { get; set; }
+    }
+}
---
+++ PersonalGoalsApi/PersonalGoalsApi/Models/Goal.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalGoalsApi.Models
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public  string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
