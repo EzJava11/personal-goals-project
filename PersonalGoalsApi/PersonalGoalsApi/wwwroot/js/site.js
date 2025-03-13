339f7167b7dcc85a32310943f86897b760efe53c
JAVIER SANTIAGO NIÑO PEÑALOZA <javninpen@alu.edu.gva.es>
2025-03-13 13:14:44 +0100

    avance

--- PersonalGoalsApi/PersonalGoalsApi/wwwroot/js/site.js
+++ PersonalGoalsApi/PersonalGoalsApi/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿const uri = 'api/User';
+
+document.getElementById(logIn).addEventListener("submit", async function logIn(event) {
+    event.preventDefault();
+    const username = document.getElementById("user").value ;
+    const password = document.getElementById("pass").value ;
+
+
+    try{
+        const response = await fetch("https://localhost:5001/api/user", {
+            method : "Post",
+            headers : {"Content-Type" : "aplication/json"},
+            body: JSON.stringify({username, password})
+        });
+        
+    }
+})
